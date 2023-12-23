cc114f4fe25b14d99cb1ebc7a88a982a3af0abdc
VintageDon <donaldfountain@gmail.com>
2023-12-23 04:44:33 -0500

    initial working script

---
+++ bash-scripts/create-ubuntu2204-proxmoxv8-template.sh
@@ -0,0 +1,8 @@
+# Name: create-ubuntu2204-proxmox-template.sh
+# Description: Simple bash script to download and configure
+#              a ready-to-use VM template for Ubuntu 22.04
+#              on Proxmox v8+
+# 
+# https://github.com/vintagedon/proxmoxlab/tree/main/bash-scripts
+# License: Apache-2.0
+
